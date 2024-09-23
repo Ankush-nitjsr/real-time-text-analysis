314802c2505c519d99d2c15a6002417c45699067
Ankush <ankushnitjsr@gmail.com>
2024-09-23 23:45:17 +0530

    Update App.tsx
    Modify App component to include HomePage component and HomePage component contains all new feature files

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-import { useState } from "react";
 import "./App.css";
+import { HomePage } from "./pages/HomePage";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>
-      <h2 className="border border-black">Display Count : {count}</h2>
-      <button onClick={() => setCount((prev) => prev + 1)}>+</button>
+      <HomePage />
     </>
   );
 }
