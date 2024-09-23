2b0e4a1487f82b44b24e6b5a1694e5663ad7d808
Ankush <ankushnitjsr@gmail.com>
2024-09-23 23:57:17 +0530

    Update HomePage component
    Modify HomePage to include word/words replacement feature

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Statistics } from "../components/Statistics";
 import { TextInput } from "../components/TextInput";
+import { StringReplacement } from "../components/StringReplacement";
 
 export const HomePage = () => {
   const [text, setText] = useState("");
@@ -8,11 +9,16 @@ export const HomePage = () => {
   const handleTextChange = (newText: string) => {
     setText(newText);
   };
+
+  const handleTextReplace = (newText: string) => {
+    setText(newText);
+  };
   return (
     <>
       <h2>Message</h2>
       <TextInput text={text} onChange={handleTextChange} />
       <Statistics text={text} setText={setText} />
+      <StringReplacement text={text} onReplace={handleTextReplace} />
     </>
   );
 };
